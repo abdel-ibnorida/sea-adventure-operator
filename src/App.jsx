d7922962f6a8d204b73c2e4b068312b77af66b8a
abdel-ibnorida <114391513+abdel-ibnorida@users.noreply.github.com>
2023-08-04 12:11:12 +0200

    feature: add step five requests

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import './App.css'
 function App() {
   const [showAllCardSection, setShowAllCardSection] = useState(true);
   const [numCardsShowed, setNumCardsShowed] = useState(8);
+  const [portListToShow, setPortListToShow] = useState('');
+
   const onHandleShowMore = () => {
     if (dataTrip.length - numCardsShowed > 8) {
       setNumCardsShowed(prev => prev + 8);
@@ -17,8 +19,12 @@ function App() {
       setNumCardsShowed(dataTrip.length)
     }
   }
-  const onHandleSelect = (e) => {
-    setShowAllCardSection(!showAllCardSection);
+  const onHandleSelectView = (e) => {
+    setShowAllCardSection(e.target.value === "true" ? true : false);
+  }
+  const onHandlePortSelect = (e) => {
+    setShowAllCardSection(false);
+    setPortListToShow(e.target.value);
   }
 
   return (
@@ -31,35 +37,52 @@ function App() {
           <h1>Lorem ipsum dolor, sit amet consectet.</h1>
         </div>
       </div>
+
+
       <div>
-        <select onChange={onHandleSelect}>
-          <option value="true">Mostra tutti</option>
-          <option value="false">Mostra per porto di porto di partenza</option>
+        <select onChange={onHandleSelectView}>
+          <option value={true}>Mostra tutti</option>
+          <option value={false}>Mostra per porto di porto di partenza</option>
         </select>
       </div>
+      <div>
+        <select onChange={onHandlePortSelect} >
+          {listDeparturePort.map(el => (
+            <option value={el}>{el}</option>
+          ))}
+        </select>
+      </div>
+
       {
         showAllCardSection ?
           <div className='cards_list'>
-            {dataTrip.slice(0, numCardsShowed).map(element => (
-              <Card data={element} />
-            ))}
-            {numCardsShowed >= dataTrip.length ? '' : <button className='show_more_btn' onClick={onHandleShowMore}>Mostra altri</button>}
-          </div>
-          :
-          <div className='cards_list_group'>
             {
-              listDeparturePort.map((element => (
-                <>
-                  <div className='name_port_group'>{element}</div>
-                  <div className='cards_list'>
-                    {dataTrip.filter(trip => trip.departure.Port === element).map(el => <Card data={el} />)}
-                  </div>
-                </>
-
-              )))
+              dataTrip.slice(0, numCardsShowed).map(element => (
+                <Card data={element} />
+              ))
             }
-          </div>
-          
+            {numCardsShowed >= dataTrip.length ? '' : <button className='show_more_btn' onClick={onHandleShowMore}>Mostra altri</button>}
+          </div >
+          :
+          portListToShow !== '' ?
+            <div className='cards_list'>
+              {dataTrip.filter(el => el.departure.Port === portListToShow).map(element => <Card data={element} />)}
+            </div>
+            :
+            <div className='cards_list_group'>
+              {
+                listDeparturePort.map((element => (
+                  <>
+                    <div className='name_port_group'>{element}</div>
+                    <div className='cards_list'>
+                      {dataTrip.filter(trip => trip.departure.Port === element).map(el => <Card data={el} />)}
+                    </div>
+                  </>
+
+                )))
+              }
+            </div>
+
       }
       <div className='banner'>
         <div className='banner_overlay'>
